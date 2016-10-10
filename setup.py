a6b836ec4c246bd12f5e10cbcc78bfde9c187394
Victor Hahn - FLEXOPTIX <Victor.Hahn@flexoptix.net>
2016-10-10 14:00:40 +0200

    Shamelessly decrement major version. We're not stable yet.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='Atlassian Permissions',
-    version='1.2',
+    version='0.2',
     description='Extract Atlassian permissions',
     author='Sýlvan Heuser, Victor Hahn Castell',
     author_email='victor.hahn@flexoptix.net',
