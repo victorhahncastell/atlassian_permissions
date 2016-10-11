db06039ecb94100bbecb23b5fdee13e306458809
Victor Hahn - FLEXOPTIX <Victor.Hahn@flexoptix.net>
2016-10-11 16:33:52 +0200

    Fix big fuckup from last commit

--- atlassian/__init__.py
+++ atlassian/__init__.py
@@ -2,8 +2,6 @@ import logging
 from urllib.parse import urlsplit, urljoin
 from requests import get
 
-l = logging.getLogger(__name__)
-
 #TODO: move this somewhere sensible
 #TODO: useful error handling (CLI...)
 class HTTPClient:
@@ -13,16 +11,14 @@ class HTTPClient:
         self.password = password
 
     def get(self, url):
-        request_url =  self.base + url
-
-        l.debug("Will now get: " + str(request_url))
+        urlparts = urlsplit(url)
+        request_url = urljoin(self.base, urlparts.path)
+        if urlparts.query is not None:
+            request_url += "?" + urlparts.query
 
         if self.user is not None:
             response = get(request_url, auth=(self.user, self.password))
         else:
             response = get(request_url)
-
-        assert response.status_code is 200, 'Error when requesting {}, response code {}.'.format(request_url, response.status_code)
-        # TODO: Need better error handling
-
+        assert response.status_code is 200, 'Error when requesting {}.'.format(request_url)
         return response.json()
