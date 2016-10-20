860beed7fd105df8fd1f5e6db9b5f3fd1dd7b396
Victor Hahn - FLEXOPTIX <Victor.Hahn@flexoptix.net>
2016-10-20 17:03:52 +0200

    Amend docker-compose helper script

--- helperscripts/dockercompose_runner.sh
+++ helperscripts/dockercompose_runner.sh
@@ -5,21 +5,23 @@ rm -f permissions.html
 rm -f changes.html
 rm -f mailbody.html
 
-docker-compose build
-
 # create a plain old regular permissions report
+docker-compose build
 docker-compose run --name atlassian_permissions_run atlassian_permissions -S status.pickle --html -o permissions.html -u $username -p $password "$@"
 docker cp atlassian_permissions_run:/permissions.html ./permissions.html
+docker cp atlassian_permissions_run:/status.pickle ./status.pickle
+docker rm atlassian_permissions_run
 
 # if we have a previous state, produce a diff
-if [ -f ./status.pickle ]; then
-	docker cp ./status.pickle atlassian_permissions:/oldstatus.pickle
-	docker-compose run --name atlassian_permissions_run atlassian_permissions -L status.pickle -cmp oldstatus.pickle --html -o changes.html --diff
+if [ -f ./oldstatus.pickle ]; then
+	docker-compose build
+	docker-compose run --name atlassian_permissions_run atlassian_permissions -L /opt/atlassian_permissions/status.pickle -cmp /opt/atlassian_permissions/oldstatus.pickle --html -o changes.html --diff
 	docker cp atlassian_permissions_run:/changes.html ./changes.html
+	docker rm atlassian_permissions_run
 	cp changes.html mailbody.html      # use change report for mail notification
+	rm oldstatus.pickle
 else
 	cp permissions.html mailbody.html  # no change report available, mail full permission report
 fi
 
-# save state for next time's diff
-docker cp atlassian_permissions_run:/status.pickle ./status.pickle
+mv status.pickle oldstatus.pickle
